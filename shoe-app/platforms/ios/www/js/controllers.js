cae70fa332f0aa47de7c80450729dbfe81627885
ddayal-fiksu <ddayal@fiksu.com>
2014-08-09 10:08:21 -0400

    Add API key

--- shoe-app/platforms/ios/www/js/controllers.js
+++ shoe-app/platforms/ios/www/js/controllers.js
@@ -1,15 +1,39 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['ionic'])
+.constant('FORECASTIO_KEY', 'cbd83f782f0c29cd4175bd08d562d8d3')
+.controller('HomeCtrl', function($scope,$state,Weather,DataStore) {
+    //read default settings into scope
+    console.log('inside home');
+    $scope.city  = DataStore.city;
+    var latitude  =  DataStore.latitude;
+    var longitude = DataStore.longitude;
 
-.controller('DashCtrl', function($scope) {
+    //call getCurrentWeather method in factory ‘Weather’
+    Weather.getCurrentWeather(latitude,longitude).then(function(resp) {
+      $scope.current = resp.data;
+      console.log('GOT CURRENT', $scope.current);
+      //debugger;
+    }, function(error) {
+      alert('Unable to get current conditions');
+      console.error(error);
+    });
+
 })
+.controller('LocationsCtrl', function($scope,$state, Cities,DataStore) {
+  $scope.cities = Cities.all();
 
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
+  $scope.changeCity = function(cityId) {
+    //get lat and longitude for seleted location
+    var lat  = $scope.cities[cityId].lat; //latitude
+    var lgn  = $scope.cities[cityId].lgn; //longitude
+    var city = $scope.cities[cityId].name; //city name
+
+    DataStore.setCity(city);
+    DataStore.setLatitude(lat);
+    DataStore.setLongitude(lgn);
+
+    $state.go('tab.home');
+  }
 })
-
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
-})
-
-.controller('AccountCtrl', function($scope) {
+.controller('SettingsCtrl', function($scope) {
+    //manages app settings
 });
--- shoe-app/www/js/services.js
+++ shoe-app/www/js/services.js
@@ -1,26 +1,66 @@
-angular.module('starter.services', [])
+'use strict';
 
-/**
- * A simple example service that returns some data.
- */
-.factory('Friends', function() {
-  // Might use a resource here that returns a JSON array
+var forecastioWeather = ['$q', '$resource', '$http', 'FORECASTIO_KEY', 
+  function($q, $resource, $http, FORECASTIO_KEY) {
+  var url = 'https://api.forecast.io/forecast/' + FORECASTIO_KEY + '/';
 
-  // Some fake testing data
-  var friends = [
-    { id: 0, name: 'Scruff McGruff' },
-    { id: 1, name: 'G.I. Joe' },
-    { id: 2, name: 'Miss Frizzle' },
-    { id: 3, name: 'Ash Ketchum' }
+  var weatherResource = $resource(url, {
+    callback: 'JSON_CALLBACK',
+  }, {
+    get: {
+      method: 'JSONP'
+    }
+  });
+
+  return {
+    //getAtLocation: function(lat, lng) {
+    getCurrentWeather: function(lat, lng) {
+      return $http.jsonp(url + lat + ',' + lng + '?callback=JSON_CALLBACK');
+    }
+  }
+}];
+
+angular.module('starter.services', ['ngResource'])
+.factory('Cities', function() {
+var cities = [
+    { id: 0, name: 'Miami', lat:25.7877 , lgn: 80.2241 },
+    { id: 1, name: 'New York City' ,lat: 40.7127 , lgn: 74.0059 },
+    { id: 2, name: 'London' ,lat:51.5072 , lgn: 1.1275 },
+    { id: 3, name: 'Los Angeles' ,lat: 34.0500 , lgn: 118.2500 },
+    { id: 4, name: 'Dallas' ,lat: 32.7758 , lgn:96.7967  },
+    { id: 5, name: 'Frankfurt' ,lat:50.1117 , lgn: 8.6858 },
+    { id: 6, name: 'New Delhi' ,lat:28.6100 , lgn: 77.2300 }
   ];
 
   return {
     all: function() {
-      return friends;
+      return cities;
     },
-    get: function(friendId) {
+    get: function(cityId) {
       // Simple index lookup
-      return friends[friendId];
+      return cities[cityId];
     }
   }
-});
+}).
+factory('DataStore', function() {
+    //create datastore with default values
+    var DataStore = {
+        city:       'Miami',
+        latitude:   25.7877,
+        longitude:  80.2241 };
+
+    DataStore.setCity = function (value) {
+       DataStore.city = value;
+    };
+
+    DataStore.setLatitude = function (value) {
+       DataStore.longitude = value;
+    };
+
+    DataStore.setLongitude = function (value) {
+       DataStore.longitude = value;
+    };
+
+    return DataStore;
+})
+.factory('Weather', forecastioWeather);
